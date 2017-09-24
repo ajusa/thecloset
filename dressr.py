2bf857a601b05bb73dbfd45c8669ad8d96699af4
Lexie Roberts <bestviolingirl@gmail.com>
2017-09-24 05:09:14 -0400

    changes

--- dressr.py
+++ dressr.py
@@ -18,6 +18,7 @@ outfit = []
 first = 0
 output_outfits = []
 awesomeOutfits = []
+clothesMatchesOptions = []
 
 column = ["topColors", "bottomColors"]
 
@@ -87,7 +88,7 @@ def algorithm():
     testTops = []
     testBottoms = []
     outfits = createAllOutfits()
-    clothesMatchesOutfits = []
+    global clothesMatchesOutfits
 
     liked = '[{"type": "shorts", "color": "black", "style": "fancy"}, {"type": "shirt", "color": "blue", "style": "fancy"}]'
     top_rgb = hex_to_rgb(liked[0][0]["color"])
@@ -95,15 +96,19 @@ def algorithm():
 
     for i in range(0,outfits.__len__()):
         testTop_rgb = hex_to_rgb(outfits[i][0]["color"])
+        print(testTop_rgb)
         testLwr_rgb = hex_to_rgb(outfits[i][1]["color"])
+        print(testLwr_rgb)
 
         difTop = rgb_dif(top_rgb, testTop_rgb)
         difLwr = rgb_dif(lwr_rgb, testLwr_rgb)
+        print(difTop)
+        print(difLwr)
 
-        if difTop[0] < 51 and difTop[1] < 51 and difTop[2] < 51:
+        if difTop[0] <= 51 and difTop[1] <= 51 and difTop[2] <= 51:
             clothing_matches.append(outfits[i][0]) #type is top
 
-        if difLwr[0] < 51 and difLwr[1] < 51 and difLwr[2] < 51:
+        if difLwr[0] <= 51 and difLwr[1] <= 51 and difLwr[2] <= 51:
             clothing_matches.append(outfits[i][1])  # type is bottoms
 
     #now that we have some similar things in clothing_matches...
